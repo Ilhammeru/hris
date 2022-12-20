d4e8d41745fa5f233845bcdbd0444bde79acb519
ilhamgumilangm <ilham.gumilang.x@fyaril.com>
2022-12-21 00:25:57 +0700

    add log

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
     public function webhook(Request $request): JsonResponse
     {
         $content = $request->getContent();
+        Log::debug($content);
         return response()->json($content);
     }
 }
