dcd74a967df5042e3acff8f787a546c4d64b05b7
ilhamgumilangm <ilham.gumilang.x@fyaril.com>
2023-01-02 00:10:28 +0700

    update

--- app/Http/Services/TelegramService.php
+++ app/Http/Services/TelegramService.php
@@ -196,6 +196,7 @@ class TelegramService {
         $posistion = array_search($last_step_action, $this->waste_list_action());
         $posistion = (int) $posistion + 1;
         $function_name = 'send_' . $this->waste_list_action()[$posistion] . '_chat';
+        Log::debug($function_name);
         $this->$function_name($payload, $posistion, $msg);
     }
 
